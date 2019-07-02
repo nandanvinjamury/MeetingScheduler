7173019552063f42124d0783f1d64b88a20ac8e1
nandanvinjamury <nvinjamury@gmail.com>
2019-07-02 03:18:18 -0700

    updated employeedatabase

--- src/EmployeeDatabase.java
+++ src/EmployeeDatabase.java
@@ -1,4 +1,6 @@
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -30,37 +32,56 @@ public class EmployeeDatabase{
 		
 		Document employee = new Document("username", entryInfo.getUsername());
         employee.append("password", entryInfo.getPassword());
+
+        /*long[] meetingIDs = new long[entryInfo.getSchedule().getMeetings().length > 0 ? entryInfo.getSchedule().getMeetings().length : 1];
         
-        long[] meetingIDs = new long[entryInfo.getSchedule().getMeetings().length];
-        
-        for(int i=0; i<meetingIDs.length;i++) {
-        	meetingIDs[i] = entryInfo.getSchedule().getMeetings()[i].getID();
+        if(meetingIDs != null) {
+	        for(int i=0; i<meetingIDs.length;i++) {
+	        	if(entryInfo.getSchedule().getMeetings()[i] != null)
+	        		meetingIDs[i] = entryInfo.getSchedule().getMeetings()[i].getID();
+	        }
         }
         
         Document meetings = new Document("id", meetingIDs);
         
         
         Document schedule = new Document("meetings", meetings);
-        employee.append("schedule", schedule);
         
+        //if(schedule != null)
+        int[] array = {0,1,2,3,4};
+        schedule.append("schedule",array);
+        */
         
         employeeCollection.insertOne(employee);
-        
+        //employeeCollection.insertOne(schedule);
 	}
 	
 	public boolean FindEntry(String username) {
 		
-		//if()
-		
-		
+		Document found = (Document) employeeCollection.find(new Document("username", username)).first();
+		if(found != null) {
+			return true;
+		}		
 		
 		return false;
 	}
 	
+	public void UpdatePassword(String username, String password) {
+		
+		Document found = (Document) employeeCollection.find(new Document("username", username)).first();
+		if(found != null) {
+			Bson updatedValue = new Document("password", password);
+			Bson updatedOperation = new Document("$set", updatedValue);
+			employeeCollection.updateOne(found, updatedOperation);
+		}
+		
+	}
+	
 	
 	public void DeleteEntry(String username) {
-		if(FindEntry(username)) {
-			//delete
+		Document found = (Document) employeeCollection.find(new Document("username", username)).first();
+		if(found != null) {
+			employeeCollection.deleteOne(found);
 		} else {
 			System.out.println("Employee not found in system!");
 		}
@@ -97,9 +118,25 @@ public class EmployeeDatabase{
 		
 		man.changeSchedule(schedule);
 		
+		Employee man2 = new Employee();
+		Schedule schedule2 = new Schedule();
+		Meeting meeting2 = new Meeting();
+		Room room2 = new Room();
+		
+		room.setRoomNumber(6);
+		room.setAvailability(true);
+		
+		
+		
+		man.changeUsername("JaredDunn");
+		man.changePassword("password2");
 		
 		EmployeeDatabase db = new EmployeeDatabase();
 		db.AddEntry(man);
+		db.AddEntry(man2);
+		System.out.println(db.FindEntry("JaredDunn"));
+		System.out.println(db.FindEntry("JohnSmith"));
+		System.out.println(db.FindEntry("BobRoss"));
 		db.Close();
 		
 		
